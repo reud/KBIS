b4891783a873c7e9044cf90d6e7b86db8b842de8
reiji <31179220+reud@users.noreply.github.com>
2018-08-06 22:23:32 +0900

    Bata2

--- KBIS/Routine.py
+++ KBIS/Routine.py
@@ -52,8 +52,8 @@ class Routine(object):
         if(not self.devmode):#本環境ではファイルから読み込む(ファイルあるの前提とする。)
 
             self.logwriter.LogWrite("print","Logファイルを読み込みます。")
-            #IgnoreListFile = open('../../KBIS_Workingplace/IgnoreList.txt')
-            IgnoreListFile = open('IgnoreList.txt')
+            IgnoreListFile = open('../../KBIS_Workingplace/IgnoreList.txt')
+            #IgnoreListFile = open('IgnoreList.txt')
 
             temp = IgnoreListFile.readline().strip()
             while (temp):
